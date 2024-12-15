5e286ad6ce5992e7fd1d35b00ab9d8c63857f369
Daniel <danielazmy541@gmail.com>
2024-12-15 11:17:15 +0200

    Initial Commit

---
+++ Nooro Weather/Nooro_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nooro_WeatherApp.swift
+//  Nooro Weather
+//
+//  Created by Dodo's Mac on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nooro_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nooro WeatherTests/Nooro_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nooro_WeatherTests.swift
+//  Nooro WeatherTests
+//
+//  Created by Dodo's Mac on 15/12/2024.
+//
+
+import Testing
+@testable import Nooro_Weather
+
+struct Nooro_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
